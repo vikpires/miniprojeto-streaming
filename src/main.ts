11d9020784dd151c0e806b67616a1a204e8977c8
Beatriz Monteiro <biafigmonteiro@gmail.com>
2024-06-23 22:54:28 -0300

    add global registry with primevue components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import './assets/tailwind.css';
 
-createApp(App).use(router).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';       // Theme
+import 'primevue/resources/primevue.min.css';                 // Core CSS
+import 'primeicons/primeicons.css';  
+import ToastService from "primevue/toastservice"
+import { setComponent } from './modules/component.module';
+
+const app = createApp(App);
+app.use(router)
+app.use(PrimeVue)
+app.use(ToastService)
+setComponent(app)
+app.mount('#app')
