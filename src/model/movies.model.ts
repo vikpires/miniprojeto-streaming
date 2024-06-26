c97aef91362acf3734f1623c5cead0cb091ce722
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-06-26 12:19:46 -0300

    add movies model and series model

---
+++ src/model/movies.model.ts
@@ -0,0 +1,17 @@
+export class MoviesModel {
+    constructor(
+        public adult?: boolean,
+        public backdrop_path?: string,
+        public genre_ids?: number[],
+        public id?: number,
+        public original_language?: string,
+        public original_title?: string,
+        public overview?: string,
+        public popularity?: number,
+        public poster_path?: string,
+        public release_date?: Date,
+        public title?: string,
+        public vote_average?: number,
+        public vote_acount?: number
+    ){ }
+}
