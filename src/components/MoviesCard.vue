17d172bd6d879a861c6ef802e706f80d87bfe9c1
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-07-03 13:09:11 -0300

    add cards components

---
+++ src/components/MoviesCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="relative flex items-center justify-center p-0 md:p-5">
+        <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt={{movies.original_title}} class="opacity-100 group-hover:opacity-50 rounded-lg cursor-pointer ease-in-out duration-300">
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue';
+    import { MoviesModel } from '@/model/movies.model';
+
+    export default defineComponent({
+        name: 'MoviesCard',
+        props: {
+            movie: {
+                type: Object as PropType<MoviesModel>,
+                    required: true
+            }
+        },
+    })
+</script>
---
+++ src/components/SeriesCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="relative flex items-center justify-center p-0 md:p-5">
+        <img :src="`https://image.tmdb.org/t/p/w500/${series.poster_path}`" alt={{series.original_name}} class="opacity-100 group-hover:opacity-50 rounded-lg cursor-pointer ease-in-out duration-300">
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue';
+    import { SeriesModel } from '@/model/series.model';
+
+    export default defineComponent({
+        name: 'SeriesCard',
+        props: {
+            series: {
+                type: Object as PropType<SeriesModel>,
+                    required: true
+            }
+        }
+    })
+</script>
