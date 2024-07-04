53488055bf62b5fe159a995097acd2a742b2d68f
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-07-04 16:44:10 -0300

    add cards components

---
+++ src/components/MoviesCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="items-center flex justify-center ">
+        <card-on class="relative w-full h-full hover:opacity-50 hover:bg-[#535368] hover:p-2 ease-in-out duration-300 shadow-lg cursor-pointer rounded-xl" unstyled>
+            <template #header>
+                <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" :alt="movie.original_title" class="object-cover rounded-xl"/>
+            </template>
+        </card-on>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue';
+    import { MoviesModel } from '@/model/movies.model';
+
+
+    export default defineComponent({
+        name: 'MoviesCard',
+        props: {
+            movie: {
+                type: Object as PropType<MoviesModel>,
+                    required: true
+            }
+        },
+    })
+</script>
---
+++ src/components/SeriesCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="items-center flex justify-center ">
+        <card-on class="relative w-full h-full hover:opacity-50 hover:bg-[#535368] hover:p-2 ease-in-out duration-300 shadow-lg cursor-pointer rounded-xl" unstyled>
+            <template #header>
+                <img :src="`https://image.tmdb.org/t/p/w500/${series.poster_path}`" :alt="series.original_name" class="object-cover rounded-xl"/>
+            </template>
+        </card-on>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue';
+    import { SeriesModel } from '@/model/series.model';
+
+
+    export default defineComponent({
+        name: 'SeriesCard',
+        props: {
+            series: {
+                type: Object as PropType<SeriesModel>,
+                    required: true
+            }
+        },
+    })
+</script>
