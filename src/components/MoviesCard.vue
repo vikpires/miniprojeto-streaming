0b719a71f865653a734b8e461f65cc41e16c5533
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-07-04 16:44:10 -0300

    add cards components

---
+++ src/components/MoviesCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="relative flex items-center justify-center p-0 md:p-5">
+        <card-on class="rounded-xl hover:opacity-50 hover:bg-[#8888a0] hover:p-2 ease-in-out duration-300 shadow-lg cursor-pointer" unstyled>
+            <template #header>
+                <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" :alt="movie.original_title" class="rounded-xl"/>
+            </template>
+        </card-on>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue';
+    import { MoviesModel } from '@/model/movies.model';
+
+
+    export default defineComponent({
+        name: 'MoviesCard',
+        props: {
+            movie: {
+                type: Object as PropType<MoviesModel>,
+                    required: true
+            }
+        },
+    })
+</script>
--- src/modules/component.module.ts
+++ src/modules/component.module.ts
@@ -1,4 +1,6 @@
 import NavBar from "@/components/NavBar.vue";
+import MoviesCard from "@/components/MoviesCard.vue";
+import SeriesCard from "@/components/SeriesCard.vue";
 
 //import useToast from "primevue/usetoast" const toast = useToast()
 
@@ -7,4 +9,6 @@ import {App} from "vue";
 
 export function setComponents(app:App){
     app.component('nav-bar', NavBar)
+    app.component('movies-card', MoviesCard)
+    app.component('series-card', SeriesCard)
 }
