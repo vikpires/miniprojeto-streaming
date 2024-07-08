94358e9f4c279124b2b3997a78445f509aa1b400
Beatriz Monteiro <biafigmonteiro@gmail.com>
2024-07-08 14:24:24 -0300

    Revert "add MovieView and movieService (#26)"
    This reverts commit cbb14e945fdab4e328cb964f821bca3bc9e6f7b6.

--- src/views/Movies/MoviesView.vue
+++ src/views/Movies/MoviesView.vue
@@ -1,41 +1,11 @@
-<template>  
-  <section class="w-full justify-between">
-    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      <movies-card v-for="movie in moviesList" :key="movie.id" :movie="movie"/>
-    </div>
-  </section>
+<template>
+  <p>Filmes</p>
 </template>
-
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { MoviesModel } from '@/model/movies.model';
-import { MovieService } from '@/views/Movies/movieService';
-
 
 export default defineComponent({
-data(){
-return{
-  moviesList: [] as MoviesModel[],
-}
-},
-methods:{
-getAllData(){
-  this.movieService.allMovies.subscribe({next:(response: MoviesModel[])=>{
-    this.moviesList=response;
-  }
-  });
-  this.movieService.getMovies();
-},  
-},
-computed:{
-movieService(){
-  return new MovieService()
-}
-},
-mounted() {
-this.getAllData();
-},
+  name: 'MoviesView'
 });
 </script>
-
