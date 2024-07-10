00caf53874595b66acc5600ebda002a8baccccfb
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-07-10 17:00:13 -0300

    add favorite view and local storage

--- src/utils/favorites.util.ts
+++ src/utils/favorites.util.ts
@@ -2,41 +2,44 @@ import { SeriesModel } from "@/model/series.model";
 import { MoviesModel } from "@/model/movies.model";
 import { LocalStorage } from "./localStorage.util";
 
-const FAVORITES_KEY = 'favorites';
-
-export class FavoriteUtils{
+export class SaveFavorites {
+    private readonly FAVORITES_KEY = 'favorites';
     private _favorites: Array<SeriesModel | MoviesModel>;
 
-    constructor(){
+    constructor() {
         this._favorites = this.loadFavorites();
     }
 
-    private loadFavorites(): Array<SeriesModel | MoviesModel>{
-        return LocalStorage.getItem<Array<SeriesModel | MoviesModel>>(FAVORITES_KEY) || [];
+    private loadFavorites(): Array<SeriesModel | MoviesModel> {
+        return LocalStorage.getItem<Array<SeriesModel | MoviesModel>>(this.FAVORITES_KEY) || [];
     }
 
-    private saveFavorites(): void{
-        LocalStorage.setItem(FAVORITES_KEY, this._favorites);
+    private saveFavorites(): void {
+        LocalStorage.setItem(this.FAVORITES_KEY, this._favorites);
     }
+
     get favorites(): Array<SeriesModel | MoviesModel> {
         return this._favorites;
     }
-    set favorites(value: Array<SeriesModel | MoviesModel> ){
+
+    set favorites(value: Array<SeriesModel | MoviesModel>) {
         this._favorites = value;
         this.saveFavorites();
     }
 
-
     addFavorites(item: SeriesModel | MoviesModel): void {
-        this._favorites.push(item);
-        this.saveFavorites();
-   }
-   removeFavorite(itemId: number):void{
-    this._favorites = this._favorites.filter(item => item.id !== itemId);
-    this.saveFavorites();
-   }
-   isFavorite(itemId: number): boolean {
-    return this._favorites.some(item => item.id === itemId);
-   }
-}
+        if (item.id !== undefined && !this.isFavorite(item.id)) {
+            this._favorites.push(item);
+            this.saveFavorites();
+        }
+    }
 
+    removeFavorite(itemId: number): void {
+        this._favorites = this._favorites.filter(item => item.id !== itemId);
+        this.saveFavorites();
+    }
+
+    isFavorite(itemId: number): boolean {
+        return this._favorites.some(item => item.id === itemId);
+    }
+}
--- src/views/Favorite/FavoriteView.vue
+++ src/views/Favorite/FavoriteView.vue
@@ -1,69 +1,44 @@
 <template>
   <section class="w-full justify-between">
       <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        <movies-card v-for="movie in FavoriteMovies" :key="movie.id" :movie="movie"
-        @favorite="toggleFavoriteMovies(movie)"/>
+        <h1 class="text-white flex justify-center text-3xl">Minha Lista de Filmes</h1>
+        <movies-card v-for="movie in favoriteMovies" :key="movie.id" :movie="movie"/>
       </div>
     </section>
-    <section class="w-full justify-between">
+    <!-- <section class="w-full justify-between">
+      <h1 class="text-white flex justify-center text-3xl">Minha Lista de Séries</h1>
       <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        <series-card v-for="series in FavoriteSeries" :key="series.id" :series="series"
-        @favorite="toggleFavoriteSeries(series)"/>
+        <series-card v-for="series in favoriteSeries" :key="series.id" :series="series"/>
       </div>
-    </section>
+    </section> -->
   </template>
-
 
 <script lang="ts">
 import { MoviesModel } from '@/model/movies.model';
 import { SeriesModel } from '@/model/series.model';
+import { SaveFavorites } from '@/utils/favorites.util';  
 import { defineComponent } from 'vue';
-import { FavoriteUtils } from '@/utils/favorites.util';
-  
+
   export default defineComponent({
     data() {
       return{
-        movieList: [] as MoviesModel[],
-        seriesList: [] as SeriesModel[],
-        FavoriteUtils: new FavoriteUtils()
+        favoriteMovies: [] as MoviesModel[],
+        favoriteSeries: [] as SeriesModel[]
       };      
     },
-methods:{
-  toggleFavoriteMovie(movie: MoviesModel){
-    if(this.FavoriteUtils.isFavorite(movie.id)){
-      this.FavoriteUtils.removeFavorite(movie.id);
-    }else{
-      this.FavoriteUtils.addFavorites(movie);
-    }
-  },
-  toggleFavoriteSeries(series: SeriesModel){
-    if(this.FavoriteUtils.isFavorite(series.id)){
-      this.FavoriteUtils.removeFavorite(series.id);
-    }else{
-      this.FavoriteUtils.addFavorites(series);
-    }
-  },
-  getAllData(){
+    mounted() {
+        this.getFavorites();
+    },
+    methods:{
+      getFavorites(){
+        const saveFavorites = new SaveFavorites();
+        const allFavorites = saveFavorites.favorites;
+        
+        this.favoriteMovies = allFavorites.filter((item): item is MoviesModel => 'original_title' in item);
+        this.favoriteSeries = allFavorites.filter((item): item is SeriesModel => 'original_name' in item);
 
-          this.homeService.allMovies.subscribe({next:(response: MoviesModel[])=>{
-        this.moviesList=response;
-      }
-      });
-      this.homeService.getMovies();
-    },  
-  }
- 
-},
-computed:{
-  favoriteMovies(): MoviesModel[]{
-    return this.movieList.filter(movie => this.FavoriteUtils.isFavorite(movie.id))
-  },
-  favoriteSereies(): SeriesModel[]{
-    return this.seriesList.filter(series => this.FavoriteUtils.isFavorite(series.id));
-  }
-},
-mounted() {
-  this.getAllData();
-},
-});
+
+      },
+    }
+  })
 </script>
