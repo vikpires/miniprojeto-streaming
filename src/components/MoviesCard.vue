916e6efb4f4c4dc6407e8986b194324cda158fdf
Beatriz Monteiro <biafigmonteiro@gmail.com>
2024-07-17 23:02:22 -0300

    add MovieDetails

--- src/components/MoviesCard.vue
+++ src/components/MoviesCard.vue
@@ -1,25 +1,43 @@
 <template>
     <div class="relative flex items-center justify-center p-0 md:p-5">
-        <card-on class="rounded-xl hover:opacity-50 hover:bg-[#8888a0] hover:p-2 ease-in-out duration-300 shadow-lg cursor-pointer" unstyled>
+        <card-on @click="navigateToDetail"
+            class="rounded-xl hover:opacity-50 hover:bg-[#8888a0] hover:p-2 ease-in-out duration-300 shadow-lg cursor-pointer"
+            unstyled>
             <template #header>
-                <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" :alt="movie.original_title" class="rounded-xl"/>
+                <img v-if="movie.poster_path" :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+                    :alt="movie.original_title" class="rounded-xl" />
+                <div v-else :class="{ 'hidden': !isNotFoundImageLoaded }">
+                    <img :src="notFoundImage" alt="Image Not Found" @load="onNotFoundImageLoad"
+                        class="rounded-xl h-full object-cover" />
+                </div>
+
             </template>
         </card-on>
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, PropType } from 'vue';
-    import { MoviesModel } from '@/model/movies.model';
+import { defineComponent, PropType } from 'vue';
+import { MoviesModel } from '@/model/movies.model';
 
-
-    export default defineComponent({
-        name: 'MoviesCard',
-        props: {
-            movie: {
-                type: Object as PropType<MoviesModel>,
-                    required: true
-            }
-        },
-    })
+export default defineComponent({
+    name: 'MoviesCard',
+    data() {
+        return {
+            notFoundImage: require('@/assets/images/not_found.png'),
+            isNotFoundImageLoaded: false
+        }
+    },
+    props: {
+        movie: {
+            type: Object as PropType<MoviesModel>,
+            required: true
+        }
+    },
+    methods: {
+        navigateToDetail() {
+            this.$router.push({ path: `/movies/${this.movie.id}` });
+        }
+    }
+})
 </script>
--- src/views/Movies/movie.service.ts
+++ src/views/Movies/movie.service.ts
@@ -2,21 +2,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { MoviesRest } from "@/service/rest/movies.rest";
-import { BehaviorSubject, Observable, take } from "rxjs";
+import { Subject, Observable, take } from "rxjs";
 
 
-export class MovieService{
+export class MovieService {
 
-    constructor( private _movie = new MoviesRest()){}
-  
-    private allMovies$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
-    allMovies:Observable<any> =  this.allMovies$.asObservable();
+    constructor(
+        private _movies = new MoviesRest(),
+        private _movie = new MoviesRest()
+    ) { }
 
-    getMovies(page: number = 1):void {
-        this._movie.getMovies(page).pipe(take(1)).subscribe({
+    private allMovies$: Subject<any> = new Subject<any>();
+    allMovies: Observable<any> = this.allMovies$.asObservable();
+
+    private movie$: Subject<any> = new Subject<any>();
+    movie: Observable<any> = this.movie$.asObservable();
+
+    getMovies(page: number = 1): void {
+        this._movies.getMovies(page).pipe(take(1)).subscribe({
             next: (response) => {
                 this.allMovies$.next(response);
             },
         });
+    }
+
+    getMoviesById(id: string) {
+        this._movie.getMoviesById(id).pipe(take(1)).subscribe({
+            next: (response) => {
+                this.movie$.next(response)
+            }
+        })
     }
 }
