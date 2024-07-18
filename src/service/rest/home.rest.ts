c436343b5ad8b84c1050ae93f0f5be920578a791
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-07-18 12:25:18 -0300

    update Home View, Home Service and Home Rest

---
+++ src/service/rest/home.rest.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import { Observable } from "rxjs";
+import api from "../rxjs";
+
+export class HomeRest {
+
+    getAllMedia(page: number = 1): Observable<any> {
+        const url = `/trending/all/day?page=${page}`;
+        return api.get(url)
+    }
+}
