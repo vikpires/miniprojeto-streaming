65348dc7c0653f1f221025c96ebcec9afbbdf1c2
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-07-22 14:10:55 -0300

    fix home view (#38)

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,26 +1,25 @@
 <template>  
   <section class="w-full justify-between">
     <div class="p-4 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 md:gap-1 lg:gap-3">
-      <movies-card v-for="movie in homeList" :key="movie.id" :movie="movie"/>
-      <series-card v-for="series in homeList" :key="series.id" :series="series" />
+      <template v-for="item in homeList" :key="item.id">
+        <movies-card v-if="isMovie(item)" :movie="item"/>
+        <series-card v-else :series="item"/>
+      </template>
     </div>
     <div class="pt-14 md:pt-10"></div>
 <paginator-pages :totalRecords="totalResults" :currentPage="page" :maxPages="maxPages"
   @page-change="handlePageChange"/>
   </section>
 </template>
-
 <script lang="ts">
-
 import { defineComponent } from 'vue';
 import { HomeService } from './home.service';
 import { SeriesModel } from '@/model/series.model';
 import { MoviesModel } from '@/model/movies.model';
-
 export default defineComponent({
 data() {
 return {
-  homeList: [] as MoviesModel[] | SeriesModel[],
+  homeList: [] as (MoviesModel | SeriesModel)[],
   page: 1,
   totalResults: 0,
   totalPages: 0,
@@ -48,7 +47,10 @@ totalResultsResponse(total: number): number {
 },
 totalPagesResponse(totalPages: number): number {
   return totalPages <= this.maxPages ? totalPages : this.maxPages;
-}
+},
+isMovie(item: MoviesModel | SeriesModel): item is MoviesModel {
+      return 'original_title' in item;
+    }
 },
 computed: {
 HomeService() {
--- src/views/Home/home.service.ts
+++ src/views/Home/home.service.ts
@@ -1,15 +1,15 @@
 /* eslint-disable */
 import { HomeRest } from "@/service/rest/home.rest";
-import { BehaviorSubject, Observable, take } from "rxjs";
+import { Subject, Observable, take } from "rxjs";
 
 export class HomeService{
     constructor(
         private _allMedia = new HomeRest()
 ){}
 
-    private allMedia$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+    private allMedia$: Subject<any> = new Subject<any>();
     allMedia: Observable<any> = this.allMedia$.asObservable();
-    
+
     getAllMedia(page: number = 1): void{
         this._allMedia.getAllMedia(page).pipe(take(1)).subscribe({
             next: (response) => {
