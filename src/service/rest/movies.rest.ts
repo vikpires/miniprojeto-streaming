d0b24e349cf39bbfa9ad0df0a7cafdaa33224848
Vitor Pires <88112014+vikpires@users.noreply.github.com>
2024-07-22 17:01:34 -0300

    add trailer for movies and series

--- src/service/rest/movies.rest.ts
+++ src/service/rest/movies.rest.ts
@@ -6,7 +6,7 @@ import api from "../rxjs";
 
 export class MoviesRest {
     getMoviesById(id: string): Observable<any>{
-        const url = `/movie/${id}`;
+        const url = `/movie/${id}?append_to_response=videos`;
         return api.get(url);
     }
     getMovies(page: number = 1): Observable<any>{
